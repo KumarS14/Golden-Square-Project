9cdad45fc843e9dbfa9159065b398a6c92a9d9d3
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-15 16:53:43 +0000

    conditional to check good grammer

--- lib/improve_grammer.rb
+++ lib/improve_grammer.rb
@@ -1,5 +1,6 @@
-def grammer(text)
+def grammar(text)
  punctuation = ['.','!','?']
  if text.strip == ""
     return "ERROR, your text is empty"
-end
+ elsif text[0] == text[0].upcase && punctuation.include?(text[-1])
+    return "Good grammar"
