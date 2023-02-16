bca6acc49e534cd4167d16f1f2923cfffe5798c4
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-16 13:09:14 +0000

    restarted program added first test

--- spec/tasks_todo_spec.rb
+++ spec/tasks_todo_spec.rb
@@ -1,9 +1,7 @@
 require 'tasks_todo.rb'
-RSpec.describe "todo tasks" do
-    it "expects program to output You have no current tasks" do
-        expect(todo_tasks("#COMPLETED gym")).to eq "You have no current tasks"
+RSpec.describe "user tasks" do
+    it "expects program to output There are currently 0 tasks at the moment" do
+        expect(user_tasks("#COMPLETED swimming")).to eq "There are currently 0 tasks at the moment"
     end
-    it "expects program to output you have one current task" do
-        expect(todo_tasks("#TODO homework")).to eq "You have 1 task these are he tasks : #TODO homework"
-    end
+    
 end
