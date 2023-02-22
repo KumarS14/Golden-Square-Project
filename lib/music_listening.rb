f605c474cb37365322a35a970be389bdaef7361d
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-22 16:53:11 +0000

    creation of class

--- lib/music_listening.rb
+++ lib/music_listening.rb
@@ -1,10 +1,3 @@
-class Music_tracker
-    def initialize
-      @music = []
-    end
-    
-    def music_list(song)
-        fail "You have not entered a input" if song == ""
-        @music.push(song)
-    end
+class User_music
+
 end
