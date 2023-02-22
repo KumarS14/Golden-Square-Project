3872286cd81277396eab00088c59ca405cc38da1
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-22 16:41:41 +0000

    first test that tests for fail as no input

--- lib/music_listening.rb
+++ lib/music_listening.rb
@@ -1,3 +1,10 @@
 class Music_tracker
+    def initialize
+      @music = []
+    end
     
+    def music_list(song)
+        fail "You have not entered a input" if song == ""
+        @music.push(song)
+    end
 end
--- spec/music_listening_spec.rb
+++ spec/music_listening_spec.rb
@@ -1,2 +1,8 @@
 require "music_listening.rb"
-RSpec.describe Music_tracker
+RSpec.describe Music_tracker do
+    musics = Music_tracker.new()
+    it "returns error as user has not enterd input" do
+        expect { musics.music_list("")}.to raise_error "You have not entered a input"
+    end
+
+end
