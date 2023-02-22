ae472800274965c338a552dedba97e2ec2ce9f3c
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-22 17:00:13 +0000

    create and test for music method that puts in list

--- lib/music_listening.rb
+++ lib/music_listening.rb
@@ -1,5 +1,10 @@
 class User_music
-    def initialize(music_list)
-        @music_list = music_list
+    def initialize
+        @music_list = []
+    end
+
+    def music(song)
+        @music_list.push(song)
+        return @music_list.join (",")
     end
 end
--- spec/music_listening_spec.rb
+++ spec/music_listening_spec.rb
@@ -1,4 +1,7 @@
 require "music_listening"
 RSpec.describe User_music do
-      
+    musics = User_music.new
+    it "returns the beatles " do
+        expect(musics.music("beatles")).to eq "beatles"
+    end
 end
