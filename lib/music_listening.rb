feabb91377681660bf30130e23f8050abfc6e652
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-22 17:03:55 +0000

    test and code if user enter nothing it should fail

--- lib/music_listening.rb
+++ lib/music_listening.rb
@@ -4,6 +4,7 @@ class User_music
     end
 
     def music(song)
+        fail "You have not entered input" if song == ""
         @music_list.push(song)
         return @music_list.join (",")
     end
--- spec/music_listening_spec.rb
+++ spec/music_listening_spec.rb
@@ -1,6 +1,10 @@
 require "music_listening"
 RSpec.describe User_music do
     musics = User_music.new
+    it "outputs error message as there is no input" do
+      expect { musics.music("")}.to raise_error "You have not entered input"
+
+    end
     it "returns the beatles " do
         expect(musics.music("beatles")).to eq "beatles"
     end
