91466fa4a3d1478810c2e0ca9b5edb4aa5d4f94a
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-24 16:52:59 +0000

    test if task can take a single input

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Todo do
       todo = Todo.new("")
       expect(todo.task).to eq ""
     end
+    it "tests to see if task returns swimming" do
+        todo = Todo.new("swimming")
+        expect(todo.task).to eq "swimming"
+      end
 end
