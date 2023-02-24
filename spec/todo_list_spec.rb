601e047c0dec4b343252e8a2123f54306a1b7476
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-24 18:06:20 +0000

    initialization test for incomplete test

--- spec/todo_list_spec.rb
+++ spec/todo_list_spec.rb
@@ -1,7 +1,14 @@
 require "todo_list.rb"
 RSpec.describe TodoList do
-    it "returns empty list" do
-        todo_it = TodoList.new
-        expect(todo_it.incomplete).to eq []
+    context "initilization test" do
+        it "returns empty list incomplete test" do
+            todo_it = TodoList.new
+            expect(todo_it.incomplete).to eq []
+        end
+        it "returns empty list incomplete test" do
+            todo_it = TodoList.new
+            expect(todo_it.complete).to eq []
+        end
     end
+        
 end
