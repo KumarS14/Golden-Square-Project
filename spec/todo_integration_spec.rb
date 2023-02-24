b88af0c0b4cd1bab3fd1987eb6aaf2ae0a091825
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-24 17:46:14 +0000

    integration test to see if 3 values are integrated

--- spec/todo_integration_spec.rb
+++ spec/todo_integration_spec.rb
@@ -2,13 +2,23 @@ require "todo_list.rb"
 require "todo.rb"
 RSpec.describe "Integration testing" do
     context "when we add to list" do
-        it "should return list values" do
+        it "should return two list values" do
             to_do = TodoList.new
             task = Todo.new("swimming")
             task1 = Todo.new("running")
             to_do.add(task)
             to_do.add(task1)
             expect(to_do.incomplete).to eq [task,task1]
+        end
+        it "should return three list values" do
+            to_do = TodoList.new
+            task = Todo.new("swimming")
+            task1 = Todo.new("running")
+            task2 = Todo.new("jogging")
+            to_do.add(task)
+            to_do.add(task1)
+            to_do.add(task2)
+            expect(to_do.incomplete).to eq [task,task1,task2]
         end
     end
 end
