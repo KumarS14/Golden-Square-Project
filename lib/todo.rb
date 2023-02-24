3c44a9487f532093356bf7d0e7cc53eb2ca17718
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-24 19:11:22 +0000

    made mark variable so users can say what they done

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,18 +1,20 @@
 class Todo
     def initialize(task) # task is a string
-       @task = task
+       @task = task 
     end
   
     def task
-      return @task
+      return @task.to_s
     end
   
-    def mark_done!
+    def mark_done!(mark)
+        @mark = mark
       # Marks the todo as done
       # Returns nothing
     end
   
     def done?
+        return @mark
       # Returns true if the task is done
       # Otherwise, false
     end
