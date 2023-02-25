d4ca18759f3704e01cf8e36580511cf8cb1935ab
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-25 10:58:34 +0000

    code for todo incomplete method

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -1,18 +1,26 @@
 class TodoList
     def initialize
         @todos = []
+        @tasks = []
     end
   
     def add(todo) # todo is an instance of Todo
-      @todos << todo
+      @tasks.push todo
     end
   
     def incomplete
-      return @todos
+      incomplete = []
+      @tasks.each do |a_task|
+        if a_task.mark_done!
+          incomplete.push(a_task.task)
+        end
+      end
+
+        
     end
   
     def complete
-      # Returns all complete todos
+      
     end
   
     def give_up!
